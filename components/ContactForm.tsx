e1b20f81f4284b605a07b613e51f1e2804c0ebba
mycargosync <mycargosync@gmail.com>
2026-07-09 04:52:41 +0530

    Update ContactForm.tsx

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -157,12 +157,6 @@ export default function ContactForm() {
                   value: "info@mycargosync.com",
                   href: "mailto:info@mycargosync.com",
                 },
-                {
-                  icon: (
-                    <svg viewBox="0 0 20 20" className="w-5 h-5 fill-current">
-                      <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" />
-                    </svg>
-                  )
               ].map((c) => (
                 <a
                   key={c.label}
