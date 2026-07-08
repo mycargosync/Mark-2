6af2aa6626f3e451ed2a870268c23e4e5442d7a0
mycargosync <mycargosync@gmail.com>
2026-07-09 04:41:39 +0530

    Update ContactForm.tsx

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -162,11 +162,7 @@ export default function ContactForm() {
                     <svg viewBox="0 0 20 20" className="w-5 h-5 fill-current">
                       <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" />
                     </svg>
-                  ),
-                  label: "Office",
-                  value: "1307-235 Red Embers Way NE, Calgary AB T3N 1E9, Canada",
-                  href: "#",
-                },
+                  )
               ].map((c) => (
                 <a
                   key={c.label}
